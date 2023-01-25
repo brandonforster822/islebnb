3476fffc0938264d35e3486173276fe4aea20f24
brandonforster822 <brandonforster822@gmail.com>
2023-01-24 20:30:11 -0600

    top half of navbar styled

--- islebnb-react-app/src/components/Navbar/Navbar.js
+++ islebnb-react-app/src/components/Navbar/Navbar.js
@@ -13,14 +13,23 @@ const Navbar = () => {
                     <h1>islebnb</h1>
                 </div>
                 <div className='navbar__main__search'>
-
+                    <div className='search__button__left'>
+                        <p>Anywhere</p>
+                    </div>
+                    <div className='search__button__center'>
+                        <p>Any week</p>
+                    </div>
+                    <div className='search__button__right'>
+                        <p>Add guests</p>
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </div>
                 </div>
                 <div className='navbar__main__account'>
                     <p>Islebnb your island</p>
                     <i class="fa-solid fa-globe"></i>
                     <div className='navbar__account__button'>
-                        {/* <i class="fa-solid fa-bars"></i>
-                        <img src='https://2023.riverlegacy.org/wp-content/uploads/2021/07/blank-profile-photo.jpeg'/> */}
+                        <i class="fa-solid fa-bars"></i>
+                        <img src='https://2023.riverlegacy.org/wp-content/uploads/2021/07/blank-profile-photo.jpeg'/>
                     </div>
                 </div>
             </div>

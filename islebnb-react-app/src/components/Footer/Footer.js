9c1eeda00bf601bf6ef414de0dd194bd7aa50e9e
brandonforster822 <brandonforster822@gmail.com>
2023-02-12 23:57:05 -0600

    footer resources add

--- islebnb-react-app/src/components/Footer/Footer.js
+++ islebnb-react-app/src/components/Footer/Footer.js
@@ -8,6 +8,9 @@ const Footer = () => {
                 <i class="fa-solid fa-umbrella-beach"></i>
                 <p>2023 Islebnb, Inc.</p>
             </div>
+            <div>
+                <p>Resources</p>
+            </div>
         </div>
     )
 }
